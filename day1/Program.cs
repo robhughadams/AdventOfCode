c0f418322d2633ef6479955b99fadfd7616c8512
Robert Adams <robert.adams@just-eat.com>
2021-12-01 21:02:30 +0000

    Part one.

---
+++ day1/Program.cs
@@ -0,0 +1,29 @@
+﻿var lines = File.ReadAllLines("input");
+
+var increases = 0;
+
+int? lastDepth = null;
+foreach (var line in lines)
+{
+    var depth = int.Parse(line);
+
+    string message;
+    if (lastDepth == null)
+    {
+        message = "N/A - no previous measurement";
+    }
+    else if (depth > lastDepth)
+    {
+        message = "increased";
+        increases++;
+    }
+    else
+    {
+        message = "decreased";
+    }
+
+    Console.WriteLine($"{line} ({message})");
+    lastDepth = depth;
+}
+
+Console.WriteLine($"There were {increases} increases");
