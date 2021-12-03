818b52df5e8a3bfde3c5839e8d19ed099cd4e963
Robert Adams <robert.adams@just-eat.com>
2021-12-03 15:11:57 +0000

    Day 2 part 1.

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input");
+
+const string forward = "forward";
+const string down = "down";
+const string up = "up";
+
+var depth = 0;
+var horizontalPosition = 0;
+
+foreach (var line in lines)
+{
+    if (line.StartsWith(forward))
+    {
+        var x = int.Parse(line[forward.Length..]);
+        horizontalPosition += x;
+    }
+    else if (line.StartsWith(down))
+    {
+        var x = int.Parse(line[down.Length..]);
+        depth += x;
+    }
+    else if (line.StartsWith(up))
+    {
+        var x = int.Parse(line[up.Length..]);
+        depth -= x;
+    }
+}
+
+Console.WriteLine("Depth is: " + depth);
+Console.WriteLine("Horizontal Position is: " + horizontalPosition);
+
+Console.WriteLine("Product of depth and horizontalPosition is: " + depth * horizontalPosition);
