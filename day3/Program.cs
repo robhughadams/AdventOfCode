070d922b22c915bada9ddbfa6fe68429db5a37e4
Robert Adams <robert.adams@just-eat.com>
2021-12-06 00:13:26 +0000

    Day 3 part 2.

--- day3/Program.cs
+++ day3/Program.cs
@@ -2,50 +2,63 @@
 
 var lineLength = lines[0].Length; // all lines are the same length
 
-var bitCounts = new int[lineLength];
-
-foreach (var line in lines)
+static string FindValue(
+    List<string> startLines,
+    int lineLength,
+    Func<int, int, char> valueSelector)
 {
+    var linesToKeep = new List<string>();
+
     for (var i = 0; i < lineLength; i++)
     {
-        if (line[i] == '1')
-            bitCounts[i]++;
+        var numberOfOnes = 0;
+        var numberOfZeros = 0;
+        foreach (var line in startLines)
+        {
+            if (line[i] == '1')
+                numberOfOnes++;
+            else
+                numberOfZeros++;
+        }
+
+        char valueToKeep = valueSelector(numberOfOnes, numberOfZeros);
+        Console.WriteLine("Value to keep is: " + valueToKeep);
+
+        foreach (var line in startLines)
+        {
+            if (line[i] == valueToKeep)
+                linesToKeep.Add(line);
+        }
+
+        if (linesToKeep.Count == 1)
+            break;
+
+        startLines = linesToKeep;
+        linesToKeep = new List<string>();
     }
+
+    Console.WriteLine("Number of lines to keep: " + linesToKeep.Count);
+
+    Console.WriteLine("Value is: " + linesToKeep[0]);
+
+    return linesToKeep[0];
 }
 
-Console.WriteLine("Bit counts are: " + string.Join(", ", bitCounts));
-Console.WriteLine("Line count is: " + lines.Length);
+var oxygenValue = FindValue(
+    lines.ToList(),
+    lineLength,
+    (numberOfOnes, numberOfZeros) => numberOfOnes >= numberOfZeros ? '1' : '0'
+);
+var oxygen = Convert.ToInt32(oxygenValue, 2);
 
-var halfwayPoint = lines.Length / 2;
-Console.WriteLine("Halfway point is: " + halfwayPoint);
+var co2Value = FindValue(
+    lines.ToList(),
+    lineLength,
+    (numberOfOnes, numberOfZeros) => numberOfOnes >= numberOfZeros ? '0' : '1'
+);
+var co2 = Convert.ToInt32(co2Value, 2);
 
-var gammaValues = new char[lineLength];
-var epsilonValues = new char[lineLength];
+Console.WriteLine("Oxygen generator rating is: " + oxygen);
+Console.WriteLine("CO2 scrubber rating: " + co2);
 
-for (var i = 0; i < lineLength; i++)
-{
-    if (bitCounts[i] > halfwayPoint)
-    {
-        gammaValues[i] = '1';
-        epsilonValues[i] = '0';
-    }
-    else
-    {
-        gammaValues[i] = '0';
-        epsilonValues[i] = '1';
-    }
-}
-
-var gammaString = new string(gammaValues);
-var epsilonString = new string(epsilonValues);
-
-Console.WriteLine("Gamma string is: " + gammaString);
-Console.WriteLine("Epsilon string is: " + epsilonString);
-
-var gamma = Convert.ToInt32(gammaString, 2);
-var epsilon = Convert.ToInt32(epsilonString, 2);
-
-Console.WriteLine("Gamma is: " + gamma);
-Console.WriteLine("Epsilon is: " + epsilon);
-
-Console.WriteLine("Product is: " + gamma * epsilon);
+Console.WriteLine("Product is: " + oxygen * co2);
