f48c3ed002422f4a827c932087ea8e86d846c1e8
Robert Adams <robert.adams@just-eat.com>
2021-12-07 00:58:33 +0000

    Formatting

--- day1/Program.cs
+++ day1/Program.cs
@@ -7,70 +7,70 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-		var lines = File.ReadAllLines("input");
+        var lines = File.ReadAllLines("input");
 
-		var increases = 0;
+        var increases = 0;
 
-		int? lastDepth = null;
-		for (var i = 0; i < lines.Length - 2; i++)
-		{
-			static int P(string line) => int.Parse(line);
+        int? lastDepth = null;
+        for (var i = 0; i < lines.Length - 2; i++)
+        {
+            static int P(string line) => int.Parse(line);
 
-			var depth = P(lines[i])
-				+ P(lines[i + 1])
-				+ P(lines[i + 2]);
+            var depth = P(lines[i])
+                + P(lines[i + 1])
+                + P(lines[i + 2]);
 
-			string message;
-			if (lastDepth == null)
-			{
-				message = "N/A - no previous sum";
-			}
-			else if (depth > lastDepth)
-			{
-				message = "increased";
-				increases++;
-			}
-			else
-			{
-				message = "decreased";
-			}
+            string message;
+            if (lastDepth == null)
+            {
+                message = "N/A - no previous sum";
+            }
+            else if (depth > lastDepth)
+            {
+                message = "increased";
+                increases++;
+            }
+            else
+            {
+                message = "decreased";
+            }
 
-			var window = GetWindow(i);
-			Console.WriteLine($"{window}: {depth} ({message})");
-			lastDepth = depth;
-		}
+            var window = GetWindow(i);
+            Console.WriteLine($"{window}: {depth} ({message})");
+            lastDepth = depth;
+        }
 
-		Console.WriteLine($"There were {increases} increases");
+        Console.WriteLine($"There were {increases} increases");
 
     }
 
-	private static string GetWindow(int i)
-	{
-		var list = new LinkedList<int>();
+    private static string GetWindow(int i)
+    {
+        var list = new LinkedList<int>();
 
-		const int @base = 26;
+        const int @base = 26;
 
-		i++;
+        i++;
 
-		while (i > @base)
-		{
-		int value = i % @base;
-		if (value == 0)
-		{
-			i = i / @base - 1;
-			list.AddFirst(@base);
-		}
-		else
-		{
-			i /= @base;
-			list.AddFirst(value);
-		}
-		}
+        while (i > @base)
+        {
+            int value = i % @base;
+            if (value == 0)
+            {
+                i = i / @base - 1;
+                list.AddFirst(@base);
+            }
+            else
+            {
+                i /= @base;
+                list.AddFirst(value);
+            }
+        }
 
-		if (i > 0)
-		{
-		list.AddFirst(i);
-		}
-		return new string(list.Select(s => (char)('A' + s - 1)).ToArray());
-	}
+        if (i > 0)
+        {
+            list.AddFirst(i);
+        }
+        return new string(list.Select(s => (char)('A' + s - 1)).ToArray());
+    }
 }
