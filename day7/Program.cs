d7c4099697123ae92212e472f2d6958c53cde04f
Robert Adams <robert.adams@just-eat.com>
2021-12-08 00:36:32 +0000

    Day 7 part 1.

---
+++ day7/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = File.ReadAllLines("input");
+
+var positions = lines[0].Split(',')
+    .Select(x => int.Parse(x));
+
+var maxPosition = positions.Max();
+
+var minCost = int.MaxValue;
+var bestPos = -1;
+for (var i = 0; i <= maxPosition; i++)
+{
+    var cost = 0;
+    foreach (var position in positions)
+    {
+        cost += Math.Abs(position - i);
+    }
+
+    Console.WriteLine($"Cost for position {i} is {cost}");
+    if (cost < minCost)
+    {
+        minCost = cost;
+        bestPos = i;
+    }
+}
+
+Console.WriteLine($"Position with lowest cost is {bestPos} with cost {minCost}");
