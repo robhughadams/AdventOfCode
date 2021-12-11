5ced38b95f38f4c565337a3ea621554c566bfc5a
Robert Adams <robert.adams@just-eat.com>
2021-12-11 23:08:50 +0000

    Day 9 part 2.

--- day9/Program.cs
+++ day9/Program.cs
@@ -5,51 +5,105 @@ var map = new HeightMap(lines);
 Console.WriteLine("Map height: " + map.Height);
 Console.WriteLine("Map width: " + map.Width);
 
-var totalRisk = 0;
+var basins = new List<Basin>();
+
 for (var y = 0; y < map.Height; y++)
 {
+	var basin = new Basin();
+	basins.Add(basin);
+
 	for (var x = 0; x < map.Width; x++)
 	{
-		var point = map.GetValueAt(x    , y    );
-
-		var up    = map.GetValueAt(x    , y - 1);
-		var down  = map.GetValueAt(x    , y + 1);
-		var left  = map.GetValueAt(x - 1, y    );
-		var right = map.GetValueAt(x + 1, y    );
-
-		if (point < up && point < down && point < left && point < right)
+		var point = map.GetPointAt(x, y);
+		if (point.Value < 9)
 		{
-			var risk = point + 1;
-			totalRisk += risk;
-			Console.WriteLine($"Low point: {x},{y} {point} risk: {risk}");
+			var previousRowBasin = map.GetPointAt(x, y - 1).Basin;
+			if (previousRowBasin != null && previousRowBasin != basin)
+			{
+				// Console.WriteLine($"Combining basins x: {x} y: {y} value: {point.Value}");
+				// Console.WriteLine($"Previous row basin size: {previousRowBasin.Size} values: {string.Join(',', previousRowBasin.Points.Select(p => p.Value))}");
+				previousRowBasin.Add(basin);
+				basins.Remove(basin);
+				basin = previousRowBasin;
+			}
+
+			// Console.WriteLine($"Adding point x: {x} y: {y} value: {point.Value}");
+			basin.Add(point);
+			// Console.WriteLine($"Basin size: {basin.Size} values: {string.Join(',', basin.Points.Select(p => p.Value))}");
+		}
+		else
+		{
+			basin = new Basin();
+			basins.Add(basin);
 		}
 	}
 }
 
-Console.WriteLine("Total risk is: " + totalRisk);
+Console.WriteLine("Number of basins: " + basins.Count(b => b.Size > 0));
+
+var answer = basins
+	.Select(b => b.Size)
+	.OrderByDescending(s => s)
+	.Take(3)
+	.Aggregate(1, (x, y) => x * y);
+Console.WriteLine("Product of 3 largest basins: " + answer);
+
+class Point
+{
+	public int Value { get; }
+
+	public Basin? Basin { get; set; }
+
+	public Point(int value)
+	{
+		Value = value;
+	}
+}
+
+class Basin
+{
+	public HashSet<Point> Points { get; } = new HashSet<Point>();
+
+	public int Size => Points.Count;
+
+	public void Add(Point point)
+	{
+		point.Basin = this;
+		Points.Add(point);
+	}
+
+	public void Add(Basin basin)
+	{
+		foreach (var point in basin.Points)
+		{
+			Add(point);
+		}
+	}
+}
 
 class HeightMap
 {
 	public int Height { get; }
 	public int Width { get; }
 
-	private readonly string[] _lines;
+	private readonly Point[][] _points;
 
 	public HeightMap(string[] lines)
 	{
 		Height = lines.Length;
 		Width = lines[0].Length;
-		_lines = lines;
+		_points = lines
+			.Select(l => l
+				.Select(p => new Point(int.Parse(p.ToString())))
+			    .ToArray())
+		    .ToArray();
 	}
 
-	public int GetValueAt(int x, int y)
+	public Point GetPointAt(int x, int y)
 	{
 		if (x < 0 || y < 0 || x >= Width || y >= Height)
-			return int.MaxValue;
+			return new Point(int.MaxValue);
 
-		var value = int.Parse(_lines[y][x].ToString());
-		// Console.WriteLine($"Value at: {x},{y} = {value}");
-
-		return value;
+		return _points[y][x];
 	}
 }
