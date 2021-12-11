b62a841c7047bcc4ac5bc381c486bc30e4880b7a
Robert Adams <robert.adams@just-eat.com>
2021-12-11 23:37:18 +0000

    Day 10 part 1.

---
+++ day10/Program.cs
@@ -0,0 +1,41 @@
+﻿var lines = File.ReadAllLines("input");
+
+const string openingChars = "([{<";
+const string closingChars = ")]}>";
+
+var score = 0;
+foreach (var line in lines)
+{
+	var stack = new Stack<char>();
+
+	foreach (var c in line)
+	{
+		if (openingChars.Contains(c))
+		{
+			// Console.WriteLine("Pushing: " + c);
+			stack.Push(c);
+		}
+		if (closingChars.Contains(c))
+		{
+			// Console.WriteLine("Closing char: " + c);
+			var opener = stack.Pop();
+			// Console.WriteLine("Poping: " + opener);
+
+			if (openingChars.IndexOf(opener) != closingChars.IndexOf(c))
+			{
+				Console.WriteLine($"Syntax Error: Expected {closingChars[openingChars.IndexOf(opener)]}, but found {c}");
+				score += c switch
+				{
+					')' => 3,
+					']' => 57,
+					'}' => 1197,
+					'>' => 25137,
+					_ => 0
+				};
+				break;
+			}
+		}
+	}
+}
+
+Console.WriteLine("Score: " + score);
