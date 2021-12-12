4c7c7f95e2253221bcf912122a2a1785310edc79
Robert Adams <robert.adams@just-eat.com>
2021-12-12 22:43:37 +0000

    Day 12 part 1.

---
+++ day12/Program.cs
@@ -0,0 +1,53 @@
+﻿var lines = File.ReadAllLines("input");
+
+var nodes = new Dictionary<string, List<string>>();
+
+void AddVertex(string start, string end)
+{
+	nodes.TryAdd(start, new List<string>());
+	nodes[start].Add(end);
+}
+	
+foreach (var line in lines)
+{
+	var parts = line.Split('-');
+	var a = parts[0];
+	var b = parts[1];
+
+	AddVertex(a, b);
+	AddVertex(b, a);
+}
+
+var paths = new List<List<string>>();
+
+FindPath("start", new List<string>());
+
+void FindPath(string node, List<string> path)
+{
+	paths.Add(path);
+
+	path.Add(node);
+
+	if (node == "end")
+		return;
+
+	var linkedNodes = nodes[node].Where(n => !(FirstCharIsLower(n) && path.Contains(n)));
+
+	foreach (var linkedNode in linkedNodes)
+	{
+		FindPath(linkedNode, path.ToList());
+	}
+}
+
+paths = paths.Where(p => p.Last() == "end").ToList();
+foreach (var path in paths)
+{
+	Console.WriteLine(string.Join(',', path));
+}
+
+Console.WriteLine("Number of paths: " + paths.Count);
+
+static bool FirstCharIsLower(string s)
+{
+	return char.IsLower(s[0]);
+}
