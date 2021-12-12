967b3564dc8e2587068d70aab741a67ee00e62da
Robert Adams <robert.adams@just-eat.com>
2021-12-12 00:17:27 +0000

    Day 10 part 2.

--- day10/Program.cs
+++ day10/Program.cs
@@ -3,11 +3,12 @@
 const string openingChars = "([{<";
 const string closingChars = ")]}>";
 
-var score = 0;
+var scores = new List<long>();
 foreach (var line in lines)
 {
 	var stack = new Stack<char>();
 
+	var invalid = false;
 	foreach (var c in line)
 	{
 		if (openingChars.Contains(c))
@@ -23,19 +24,37 @@ foreach (var line in lines)
 
 			if (openingChars.IndexOf(opener) != closingChars.IndexOf(c))
 			{
-				Console.WriteLine($"Syntax Error: Expected {closingChars[openingChars.IndexOf(opener)]}, but found {c}");
-				score += c switch
-				{
-					')' => 3,
-					']' => 57,
-					'}' => 1197,
-					'>' => 25137,
-					_ => 0
-				};
+				invalid = true;
 				break;
 			}
 		}
 	}
+
+	if (invalid)
+		continue;
+
+	var score = 0L;
+	foreach (var c in stack)
+	{
+		Console.Write(c);
+		score *= 5;
+		score += c switch
+		{
+			'(' => 1,
+			'[' => 2,
+			'{' => 3,
+			'<' => 4,
+			_ => 0
+		};
+	}
+	if (score > 0)
+	{
+		Console.WriteLine(" : " + score);
+		scores.Add(score);
+	}
 }
 
-Console.WriteLine("Score: " + score);
+scores.Sort();
+Console.WriteLine("Number of scores: " + scores.Count);
+var middleScore = scores[scores.Count / 2];
+Console.WriteLine("Middle score: " + middleScore);
